e7bed076f3d87124c81f0fe14c18d5b4aa629be1
xielbers <salvadorrodrig568@gmail.com>
2024-07-18 11:08:46 -0300

    18

--- index.js
+++ index.js
@@ -3,25 +3,7 @@ function VideoID(videoId) {
     window.location.href = 'player/player.html';
 }
 
-function InAPP(versionCliente) {
-    var Elemento = document.getElementById('down');
-    var Download = document.getElementById('appActualizacion');
-
-    var versionNecesaria = '18/7/24';
-    if(versionCliente == '' || versionCliente == null) {
-        location.reload();
-    } else {
-        // Verificar si el elemento existe y si la versión del cliente es correcta
-        if (Elemento && versionCliente === versionNecesaria) {
-        // Eliminar el elemento
-        Elemento.parentNode.removeChild(Elemento);
-        console.log('Versión correcta');
-        } else {
-        console.error('Versión incorrecta');
-        // Evitar el scroll
-        document.body.style.overflow = 'hidden';
-        // Mostrar el div de actualización
-        Download.style.display = 'flex';
-        }
-    }
+function VideoClear(videoId) {
+    localStorage.removeItem('videoTime_' + videoId);
+    alert('Tiempo guardado eliminado.');
 }
