131f1e15e246e0ee5bb09650dd2d92b781515b2b
xielbers <salvadorrodrig568@gmail.com>
2024-09-23 14:11:19 -0300

    23

--- Usuarios/usuarios.js
+++ Usuarios/usuarios.js
@@ -1,3 +1,13 @@
+function VideoID(videoId) {
+    localStorage.setItem('videoId', videoId);
+    window.location.href = '../player/player.html';
+}
+
+function VideoClear(videoId) {
+    localStorage.removeItem('videoTime_' + videoId);
+    alert('Tiempo guardado eliminado.');
+}
+
 let savedPass = localStorage.getItem('pass');
 let currentLocation = window.location.href;
 
--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-function VideoID(videoId) {
-    localStorage.setItem('videoId', videoId);
-    window.location.href = 'player/player.html';
-}
-
-function VideoClear(videoId) {
-    localStorage.removeItem('videoTime_' + videoId);
-    alert('Tiempo guardado eliminado.');
-}
-
 function LogIn() {
     var contraseñas = ['110528', '298465']; // Las contraseñas deben ser cadenas
 
