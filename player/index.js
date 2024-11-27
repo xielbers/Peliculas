3689627cf64b96a9db879d30d1faf076af7913f6
xielbers <salvadorrodrig568@gmail.com>
2024-11-27 14:41:18 -0300

    27

--- player/index.js
+++ player/index.js
@@ -14,7 +14,7 @@ if ('serviceWorker' in navigator) {
     var video = document.getElementById('playerVideo');
   
     // Maneja la falta videoId
-
+  
     if (!videoId) {
       alert('No se encontró el ID del video.');
       window.location.href = '../index.html';
@@ -29,6 +29,38 @@ if ('serviceWorker' in navigator) {
       videoSrc = `https://pixeldrain.com/api/file/${videoId}`;
       video.src = filmSrc;
       video.style = 'display: flex';
+  
+      // Obtener el tiempo guardado
+      const uniqueKey = `${videoId}_${plataforma}`;
+      const savedTime = localStorage.getItem(uniqueKey);
+  
+      // Si hay un tiempo guardado, restar 15 segundos y establecerlo como el tiempo actual
+      if (savedTime) {
+        video.currentTime = Math.max(0, savedTime - 15);
+      }
+  
+      // Agregar los controles de teclado
+      document.addEventListener('keydown', function(event) {
+        switch (event.key) {
+          case 'ArrowLeft':
+            video.currentTime -= 10;
+            break;
+          case 'ArrowRight':
+            video.currentTime += 10;
+            break;
+          case 'Enter':
+          case ' ':
+            if (video.paused) {
+              video.play();
+            } else {
+              video.pause();
+            }
+            break;
+        }
+  
+        // Guardar el tiempo actual al realizar cualquier acción
+        localStorage.setItem(uniqueKey, video.currentTime);
+      });
     } else if (plataforma === 'drive') {
       videoSrc = `https://drive.google.com/file/d/${videoId}/preview`;
       iframe.src = filmSrc;
@@ -37,4 +69,9 @@ if ('serviceWorker' in navigator) {
       console.error(`Plataforma '${plataforma}' no reconocida.`);
       return;
     }
+  
+    window.addEventListener('beforeunload', function(event) {
+      event.preventDefault();
+      event.returnValue = '¿Seguro que quieres salir de la página?';
+    });
   });
